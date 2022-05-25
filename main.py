8d41454c22fdc194ab58183927f79933b32f087f
dfhvc <anton25348@gmail.com>
2022-05-25 18:42:51 +0300

    Ну тепер точно єлаба

--- main.py
+++ main.py
@@ -8,72 +8,77 @@ class Main:
 
         self.nodes_num = 0
 
-        self.window = Tk()
-        self.window.configure(bg='#FFFF99')
-        self.menu = Menu(self.window)
+        self.win = Tk()
+        self.win.configure(bg='#FFFF99')
+        self.win.geometry('500x600')
+        self.win.title('Лабораторна робота №4')
+        self.win.resizable(False, False)
+        self.menu = Menu(self.win)
 
-        self.info = Label(self.window, text="Шемчук Віталій\nГрупа: ІО-13\nНомер у списку: 27\nНомер варіанту: 2",
+        self.info = Label(self.win, text="Шемчук Віталій\nГрупа: ІО-13\nНомер у списку: 27\nНомер варіанту: 2",
                           font=('Calibri', 15),
                           bg='#FFFF99')
         self.info.pack()
 
-        self.window.geometry("500x600")
-        self.list = Listbox(self.window, height=18)
 
-        self.frame1 = Frame(self.window)
+        self.frame1 = Frame(self.win)
         self.frame1.configure(bg='#FFFF99')
 
-        self.entry1 = Entry(self.frame1, width=7,
-                             bg='#FFFF99')
+        self.entry1 = Entry(self.frame1, width=5)
         self.entry1.pack()
 
         self.frame1.pack()
 
         self.but1 = Button(self.frame1, text='Задати кількість вершин',
                             command=self.nodes,
-                            bg='#FFFF99',
                             font=('Calibri', 15))
         self.but1.pack()
 
         self.but2 = Button(self.frame1, text='Задати матрицю',
-                            bg='#FFFF99',
                             command=self.matrix_making,
                             font=('Calibri', 15))
         self.but2.pack()
 
-        self.frame_2 = Frame(self.window)
+        self.frame_2 = Frame(self.win)
         self.frame_2.configure(bg='#FFFF99')
         self.frame_2.pack()
 
-        self.window.mainloop()
+        self.win.mainloop()
 
 
     def matrix_making(self):
         self.matrix_make = [[0 for i in range(self.nodes_num)] for i in range(self.nodes_num)]
 
         for i in range(self.nodes_num):
-            lab_1 = Label(self.frame_2, text=i + 1,
+            lab = Label(self.frame_2, text=i + 1,
                           bg='#FFFF99')
-            lab_1.grid(row=i + 1, column=0)
+            lab.grid(row=i + 1, column=0)
 
         for i in range(self.nodes_num):
-            lab_1 = Label(self.frame_2, text=i + 1,
+            lab = Label(self.frame_2, text=i + 1,
                           bg='#FFFF99')
-            lab_1.grid(row=0, column=i + 1)
+            lab.grid(row=0, column=i + 1)
 
         for i in range(self.nodes_num):
             for j in range(self.nodes_num):
-                self.matrix_make[i][j] = Entry(self.frame_2, width=5, bg='#FFFF99')
+                self.matrix_make[i][j] = Entry(self.frame_2, width=5)
                 self.matrix_make[i][j].grid(row=i + 1, column=j + 1)
 
-        self.but_3 = Button(self.window, text='Створити граф',
-                            bg='#FFFF99',
+        self.but_3 = Button(self.win, text='Створити граф',
                             command=self.Graph)
         self.but_3.pack()
 
-    # Метод для графу
     def Graph(self):
-        pass
+        self.edges = []
+        for j in range(self.nodes_num):
+            for i in range(self.nodes_num):
+                if self.matrix_make[j][i].get() == "1":
+                    self.edges.append([j + 1, i + 1])
+
+        G = nx.DiGraph()
+        G.add_edges_from(self.edges, color='r')
+        nx.draw(G, with_labels=True)
+        plt.show()
 
     #Метод для алгоритму
     def coloring_graph(self):
